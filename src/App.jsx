403e3ac385a9af67c9c9663212766893f1952ab0
tubez7 <thescopeboys@hotmail.com>
2023-01-04 23:52:53 +0000

    #3 add pcode feat

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,44 @@ import './App.css'
 function App() {
 
   const [areas, setAreas] = useState([]);
+  const [postcode, setPostcode] = useState("BB10");
+  const [pcodeStr, setPcodeStr] = useState("");
 
   const load = async () => {
     try {
-      const areaData = await getAreaData();
+      const areaData = await getAreaData(postcode);
       setAreas(areaData);
     } catch (error) {
       window.alert("todo: fix app")
     }
   }
 
+  const handleChange = (e) => {
+    setPcodeStr(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPostcode(pcodeStr);
+  }
+
   useEffect(() => {
     load();
-  }, []);
+  }, [postcode]);
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form className="postcode-form">
+        <fieldset>
+          <p>Please enter an outcode to search<br/>
+          eg - "M1” rather than “M1 7ED”</p>
+          <label htmlFor="postcode">Postcode: </label>
+          <input type="text" id="postcode" name="postcode" placeholder="Enter a postcode..." onChange={handleChange} /><br/>
+          <button onClick={handleSubmit}>Submit</button>
+        </fieldset>
+      </form>
+      <h2>{`Areas for ${postcode}: ${areas.length}`}</h2>
     </div>
   )
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const getAreaData = async () => {
-  const { data } = await axios.get("https://api.zippopotam.us/GB/bb10");
+export const getAreaData = async (postcode) => {
+  const { data } = await axios.get(`https://api.zippopotam.us/GB/${postcode}`);
 
   return data.places;
 };
